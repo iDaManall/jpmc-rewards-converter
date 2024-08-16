867b6aa9da67bbb16b0de428a4f8e33e6826229a
iDaManall <idamanallgt@gmail.com>
2024-08-16 12:29:02 -0400

    Task 2: modified conversion unit tests

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,17 +1,18 @@
 public class RewardValue {
     private double cashValue;
     private double milesValue;
+    public static final double conversion_rate = 0.0035;
 
     // Constructor that accepts a cash value
     public RewardValue(double cashValue){
         this.cashValue = cashValue;
-        this.milesValue = cashValue / 0.0035; // convert cash to miles
+        this.milesValue = cashValue / conversion_rate; // convert cash to miles
     }
 
     // Constructor that accepts a miles value
     public RewardValue(int milesValue){
         this.milesValue = milesValue;
-        this.cashValue = milesValue * 0.0035; // convert miles to cash
+        this.cashValue = milesValue * conversion_rate; // convert miles to cash
     }
 
     // getter method for cash value
